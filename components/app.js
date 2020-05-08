41f98c26172951ae041d93ea74a2f24f57f05543
Ben Imberman <Bimberman@gmail.com>
2020-05-08 09:27:11 -0700

    Completed the update grade functionality

--- components/app.js
+++ components/app.js
@@ -6,26 +6,53 @@ class App {
     this.gradeTable = gradeTable;
     this.pageHeader = pageHeader;
     this.gradeForm = gradeForm;
+    this.grades = [];
+
     this.computeAvg = this.computeAvg.bind(this);
+    this.editGrade = this.editGrade.bind(this);
+
+    this.getGrades = this.getGrades.bind(this);
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+
     this.createGrade = this.createGrade.bind(this);
     this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
     this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
+
     this.deleteGrade = this.deleteGrade.bind(this);
     this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
     this.handleDeleteGradeSuccess = this.handleDeleteGradeSuccess.bind(this);
+
+    this.updateGrade = this.updateGrade.bind(this);
+    this.handleUpdateGradeError = this.handleUpdateGradeError.bind(this);
+    this.handleUpdateGradeSuccess = this.handleUpdateGradeSuccess.bind(this);
   }
-  handleGetGradesError (error){
-    console.error(error);
+
+  start() {
+    this.getGrades();
+    this.gradeForm.onSubmit(this.createGrade);
+    this.gradeForm.onUpdate(this.updateGrade);
+    this.gradeTable.onDeleteClick(this.deleteGrade);
+    this.gradeTable.onEditClick(this.editGrade);
   }
-  handleGetGradesSuccess (grades){
-    this.gradeTable.updateGrades(grades);
-    this.pageHeader.updateAverage(this.computeAvg(grades));
+  computeAvg(gradesObj) {
+    let grades = 0;
+    let i = 0
+    if (gradesObj) {
+      while (gradesObj[i]) {
+        grades += gradesObj[i].grade;
+        i++;
+      }
+    }
+    return grades / i;
   }
-  getGrades(){
+  editGrade(name, course, grade, id){
+    this.gradeForm.editGradeForm(name, course, grade, id);
+  }
+
+  getGrades() {
     $.ajax({
-      url: this.baseUrl+this.path,
+      url: this.baseUrl + this.path,
       method: "GET",
       headers: {
         "X-Access-Token": this.apikey
@@ -34,22 +61,14 @@ class App {
       error: this.handleGetGradesError
     })
   }
-  start(){
-    this.getGrades();
-    this.gradeForm.onSubmit(this.createGrade);
-    this.gradeTable.onDeleteClick(this.deleteGrade);
+  handleGetGradesError (error){
+    console.error(error);
   }
-  computeAvg(gradesObj){
-    let grades = 0;
-    let i = 0
-    if(gradesObj){
-      while (gradesObj[i]){
-        grades += gradesObj[i].grade;
-        i++;
-      }
-    }
-    return grades/i;
+  handleGetGradesSuccess (grades){
+    this.gradeTable.updateGrades(grades);
+    this.pageHeader.updateAverage(this.computeAvg(grades));
   }
+
   createGrade(name, course, grade) {
     $.ajax({
       url: this.baseUrl + this.path,
@@ -72,6 +91,7 @@ class App {
   handleCreateGradeSuccess(){
     this.getGrades();
   }
+
   deleteGrade(id){
     $.ajax({
       url: this.baseUrl + this.path + id,
@@ -87,6 +107,29 @@ class App {
     console.error(error);
   }
   handleDeleteGradeSuccess(){
+    this.getGrades();
+  }
+
+  updateGrade(name, course, grade, id) {
+    $.ajax({
+      url: this.baseUrl + this.path + id,
+      method: "PATCH",
+      headers: {
+        "X-Access-Token": this.apikey
+      },
+      data: {
+        "name": name,
+        "course": course,
+        "grade": grade
+      },
+      success: this.handleUpdateGradeSuccess,
+      error: this.handleUpdateGradeError
+    })
+  }
+  handleUpdateGradeError(error) {
+    console.error(error);
+  }
+  handleUpdateGradeSuccess() {
     this.getGrades();
   }
 }
